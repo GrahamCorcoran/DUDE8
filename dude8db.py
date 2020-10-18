7e5352b213315a0e867e52ad3b23bab52b7dfa9b
Graham Corcoran <15987547+GrahamCorcoran@users.noreply.github.com>
2020-10-18 11:46:39 -0400

    Added some database helper functions, created the database.

--- dude8db.py
+++ dude8db.py
@@ -1,4 +1,5 @@
 import peewee as pw
+import datetime
 
 db = pw.SqliteDatabase('dude8.db')
 
@@ -10,7 +11,7 @@ class BaseModel(pw.Model):
 
 class Server(BaseModel):
     serverID = pw.IntegerField(primary_key=True)
-    timezone = pw.TextField()
+    timezone = pw.TextField(null=True)
     weekly_notification = pw.IntegerField(null=True)
     notification_time = pw.TimeField()
 
@@ -21,6 +22,25 @@ class DueDates(BaseModel):
     due_date = pw.DateTimeField()
 
 
+def add_duedate(server_id, description, due_date):
+    return DueDates.create(serverID=server_id,
+                           description=description,
+                           due_date=due_date)
+
+
+def remove_duedate(server_id, description):
+    query = DueDates.delete().where(serverID=server_id,
+                                    description=description)
+    return query.execute()
+
+
+def add_server(server_id):
+    return Server.create(serverID=server_id,
+                         notification_time=datetime.time(hour=8))
+
+
 if __name__ == "__main__":
     db.connect()
+    if input("Do you want to drop tables? Y/N: ").lower() in "yes":
+        db.drop_tables([Server, DueDates])
     db.create_tables([Server, DueDates])
