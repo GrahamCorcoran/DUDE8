26fb5b0e50b5c3fe01b30133b5be37988fe69921
Graham Corcoran <15987547+GrahamCorcoran@users.noreply.github.com>
2020-10-18 11:47:55 -0400

    Beginning to flesh out basic bot structure, added on_guild_join to create the server in the db.

--- main.py
+++ main.py
@@ -1,2 +1,25 @@
 import discord
+import secrets
+import dude8db
 
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('!dd'):
+        await message.channel.send('Hello!')
+
+@client.event
+async def on_guild_join(guild):
+    dude8db.add_server(guild.id)
+
+client.run(secrets.token)
