f4d15d9cfee9a0c2ee58f5b3e9494051e6310c99
Graham Corcoran <15987547+GrahamCorcoran@users.noreply.github.com>
2020-10-18 16:50:26 -0400

    Removed help embed, bot.command already handles that.

--- embeds.py
+++ embeds.py
@@ -1,15 +1,13 @@
 from discord import embeds
 
-welcome = embeds.Embed(title="DUDE8 Setup",
-                       description="To set up dude8, the timezone and your preferred "
-                                   "notification time must be set. (Default 8am) \n")
-welcome.add_field(name="Timezone",
-                  value="!dd timezone <timezone> \n Note: timezone MUST be in the format 'America/New_York, "
-                        "for a full list of Timezones see <link>",
-                  inline=False)
-welcome.add_field(name="Preferred Notification Time",
-                  value="!dd notification <h> \n"
-                        "ex: '!dd notification 8' for 8am.",
-                  inline=False)
-
-
+setup = embeds.Embed(title="DUDE8 Setup",
+                     description="To set up dude8, the timezone and your preferred "
+                                 "notification time must be set. (Default 8am) \n")
+setup.add_field(name="Timezone",
+                value="!dd timezone <timezone> \n Note: timezone MUST be in the format 'America/New_York, "
+                      "for a full list of Timezones see <link>",
+                inline=False)
+setup.add_field(name="Preferred Notification Time",
+                value="!dd notification <h> \n"
+                      "ex: '!dd notification 8' for 8am.",
+                inline=False)
