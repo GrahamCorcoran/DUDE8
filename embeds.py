f83ca9f7e27f3dc4cab9fccf1ac2640f8a342a3b
Graham Corcoran <15987547+GrahamCorcoran@users.noreply.github.com>
2020-10-19 12:56:40 -0400

    Parameterized dude8's logo url

--- embeds.py
+++ embeds.py
@@ -1,10 +1,13 @@
 from discord import embeds
 
+dude8_logo = "https://raw.githubusercontent.com/GrahamCorcoran/DUDE8/dev/images/DUDE8.jpg"
+
+
 setup = embeds.Embed(title="Setup",
                      description="To set up dude8, the timezone and your preferred "
                                  "notification time must be set. (Default 8am) \n")
 setup.set_author(name="DUDE8", url="https://github.com/GrahamCorcoran/DUDE8",
-                 icon_url="https://raw.githubusercontent.com/GrahamCorcoran/DUDE8/dev/DUDE8.jpg")
+                 icon_url=dude8_logo)
 setup.add_field(name="Timezone",
                 value="!dd timezone <timezone> \n Note: timezone MUST be in the format 'America/New_York, "
                       "for a full list of Timezones see <link>",
